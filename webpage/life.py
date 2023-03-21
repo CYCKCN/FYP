bdde53bb9ab65994f057910c9af682d545922adb
CYCKCN <cyckcn@163.com>
2023-03-21 21:06:20 +0800

    relife

--- webpage/life.py
+++ webpage/life.py
@@ -11,58 +11,7 @@ life = Blueprint('life',__name__)
 
 @life.route('/', methods=['POST', 'GET'])
 def home():
-    if current_user.is_authenticated: userStatus = True
-    else: userStatus = False
-
-    cate = request.args.get('cate')
-    maxprice = request.args.get('maxprice')
-    minprice = request.args.get('minprice')
-    search = request.args.get('search')
-
-    # print(cate, maxprice, minprice, search)
-    itemInfo = itemdb.getItemList(cate=cate if cate else "", maxprice=maxprice if maxprice else "", \
-                                  minprice=minprice if minprice else "", search=search if search else "")
-
-    if (maxprice, minprice) == ('50', '0'): price = 'Less than 50'
-    elif (maxprice, minprice) == ('100', '50'): price = 'Between 50 - 100'
-    elif (maxprice, minprice) == ('200', '100'): price = 'Between 100 - 200'
-    elif (maxprice, minprice) == ('', '200'): price = 'More than 200'
-    else: price = ""
-
-    requestInfo = requestdb.getRequestList()
-    # print(requestInfo)
-
-    if request.method == 'POST':
-        
-        button = buttonCheck(request.form)
-        if button: return button
-
-        apply = request.form.get('Apply')
-        clear = request.form.get('Clear')
-        cate = request.form.get("Category")
-        price = request.form.get("Price")
-        itemName = request.form.get("search-keyword")
-        if itemName == '' and search != '': itemName = search
-        # itemName = searchForm.search.data
-
-        if price == 'Less than 50': maxprice, minprice = '50', '0'
-        elif price == 'Between 50 - 100': maxprice, minprice = '100', '50'
-        elif price == 'Between 100 - 200': maxprice, minprice = '200', '100'
-        elif price == 'More than 200': maxprice, minprice = '', '200'
-        else: maxprice, minprice = '', ''
-
-        # print(cate, maxprice, minprice, itemName)
-
-        if clear == 'Clear':
-            return redirect(url_for("lifeverse.home", cate='', maxprice='', minprice='', search=''))
-
-        if apply == "Apply" or itemName:
-            return redirect(url_for("lifeverse.home", cate=cate, maxprice=maxprice, minprice=minprice, search=itemName))      
-
-    # print(price)
-    return render_template('home.html', search=search if search else 'Search Now', selected_cate=cate if cate else '', selected_price=price if price else '', itemInfo=itemInfo, requestInfo=requestInfo, userStatus=userStatus, itemCategories=CATEGORY, priceRange=PRICERANGE)
-
-    # return render_template('home.html')
+    return render_template('home.html')
 
 @life.route('/profile', methods=['POST', 'GET'])
 # @check_login
